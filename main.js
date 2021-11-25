0e897ab388874c6f3e9b0375ea8fbe5c44bb9d2d
imrankhanf41 <imranbutex41@gmail.com>
2021-11-25 20:26:34 +0600

    a

---
+++ main.js
@@ -0,0 +1,27 @@
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector(".display-2");
+const tempresult = document.querySelector(".temp-result");
+const numbersEl = document.querySelectorAll(".number");
+const operationEl =document.querySelectorAll(".operation");
+const equalEl =document.querySelector(".equal");
+const clearEl = document.querySelector(".all-clear");
+const lastClearEl = document.querySelector(".last-entity-clear")
+
+let dis1Num ="";
+let dis2Num ="";
+let result =null;
+let lastOperation ="";
+let haveDot = false;
+
+numbersEl.forEach(number =>{
+    number.addEventListener("click",(e)=>{
+        if(e.target.innerText === "." && !haveDot){
+            haveDot= true;
+        }
+        else if(e.target.innerText === "." && haveDot){
+            return;
+        }
+        dis2Num += e.target.innerText;
+        display2El.innerText = dis2Num;
+    })
+})
